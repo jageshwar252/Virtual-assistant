42821a46f4a36dad24e8fdb4a14a59d2e2d3accc
jageshwar252 <jageshwarkumar252@gmail.com>
2025-10-22 19:07:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-fl98.onrender.com",
     credentials: true
 }))
 app.use(express.json());
